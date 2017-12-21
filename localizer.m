ab466b4142e9a8d92e22918bb114b59e299848c6
Albert <acruz@csub.edu>
2017-12-20 23:55:58 -0800

    First localizer code

--- localizer.m
+++ localizer.m
@@ -3,60 +3,91 @@
 %   centering them on the centroid.
 
 function localizer
-global localizerSuffix
+global localizerPrefix rawPrefix
 dataLocations = { ...
-    'G:\NextCloud\grapes_2017\data\vitis_vinefera', ...
-    'G:\NextCloud\grapes_2017\data\vitis_vinifera_grapevine_yellow', ...
-    'G:\NextCloud\grapes_2017\data\vitis_vinifera_other_diseases' };
+    'control', ...
+    };
 
-localizerSuffix = 'localized';
+localizerPrefix = 'localized';
+rawPrefix = 'raw';
 
 for i=1:length(dataLocations)
     iterateLocation( cell2mat(dataLocations(i)) )
 end
 
 function iterateLocation( location )
-global localizerSuffix
+global localizerPrefix rawPrefix
 PREVDIR = cd;
 
+% Change to directory with images
 cd( location );
+% Get the images, note that dir produces a tall vector
+list = [ dir( '*.jpg' ); dir( '*.JPG' ) ];
+% Go back to root of this project
+cd( PREVDIR );
 
-list = dir( '*.jpg' );
-
+% For each image ...
 for i=1:length(list)
+    cd( location );
+    % Get the file name
     fileName = list(i).name;
-    saveDir = [ location, '\', localizerSuffix ];
+    % Use fullfile to contruct save directory name
+    saveDir = fullfile( PREVDIR, location, localizerPrefix );
     if ~exist( saveDir )
         mkdir( saveDir )
     end
-    saveFile = [ saveDir, '\', fileName ];
     
+    % Load the file
     im = imread( fileName );
-    result = localizeImage( im );
+    [ resultAugmented, n_ ] = augmentData( im );
+    for j=1:n_
+        saveFile = fullfile( saveDir, 'a', num2str(j), '-', lower(fileName) );
+        imwrite( resultAugmented(i).data, saveFile );
+        display( [ 'Writing ' saveFile ] );
+    end
     
-    imwrite( result, saveFile );
-    display( [ 'Writing ' saveFile ] );
+    cd( PREVDIR );
+end
+
+% The following function should return a struct of images that have
+% been randomly flipped and rotated for data augmentation purposes
+function [ res, NUM_AUGS ] = augmentData( im )
+NUM_AUGS = 10;
+FLIP_RATE = 0.5;
+res(1).data = [];
+
+for i=1:NUM_AUGS
+    res(i).data = localizeImage( im );
+    if rand() > FLIP_RATE
+        res(i).data = fliplr( res(i).data );
+    end
+    res(i).data = imrotate( res(i).data, rand()*360 );
 end
 
 function res = localizeImage( im )
 try
-imGray = rgb2gray( im );
-leafMask = imbinarize( imGray );
-leafMask = medfilt2(leafMask, [9 9]);
-leafMask = 1 - leafMask;
+    imGray = rgb2gray( im );
+    leafMask = imbinarize( imGray );
+    leafMask = medfilt2(leafMask, [9 9]);
+    leafMask = 1 - leafMask;
+catch e
+    error( 'Error attempting to segment leaf boundary!' );
+end
 
-%% Part 1: Determine crop conditions
-vertBound = logical(max(leafMask, [], 2 ));
-horzBound = logical(max(leafMask, [], 1 ));
-vertBound = procBound( vertBound );
-horzBound = procBound( horzBound );
+try 
+    %% Part 1: Determine crop conditions
+    vertBound = logical(max(leafMask, [], 2 ));
+    horzBound = logical(max(leafMask, [], 1 ));
+    vertBound = procBound( vertBound );
+    horzBound = procBound( horzBound );
+catch e
+    error( 'Error attempting to calculate bounds of the image!' );
+end
 
 % Later we might want to center the images
 res = im( vertBound, horzBound, : );
-catch e
-    display( 'Hmmm!' );
-end
 
+% Function selects the largest object passed
 function res = procBound( bound )
 boundLabels = bwlabel( bound );
 stats = regionprops(bound);
