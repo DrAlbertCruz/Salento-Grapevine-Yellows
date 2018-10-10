d6da3211f8541044365c75edc0be21e057b4d104
Albert <acruz37@csub.edu>
2018-10-09 17:26:43 -0700

    Lets try this again

---
+++ matlab/renameFiles.m
@@ -0,0 +1,15 @@
+% I dont wanna have to use node.js or php for the human recognition script,
+% so this MATLAB script will rename all the files in a given directory to a
+% number
+
+parpool local
+
+imagePath = '/home/acruz/data/Salento-Grapevine-Yellows-Dataset/localized/Black_rot';
+
+cd( imagePath );
+images = dir( '*.jpg' );
+
+parfor i=1:size(images,1)
+    movefile( fullfile(images(i).folder, images(i).name), ...
+       fullfile(images(i).folder, ['image-' num2str(i), '.jpg']) );
+end
